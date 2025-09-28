381d8d8668c499e778eb7fbb408c15081770d444
atatulyan <a.tatulyan@endrex.net>
2025-09-28 15:20:26 +0300

    update shared entities

---
+++ coreutils/shared/entity/__init__.py
@@ -0,0 +1,19 @@
+from .asset import Asset, AssetPair, AssetType
+from .finance_account import FinanceAccount, FinanceAccountProperties
+from .transfer import RawTransfer, Transfer
+from .types import FinanceAccountType, WalletType
+from .wallet import Wallet
+
+
+__all__ = [
+    "Asset",
+    "AssetPair",
+    "AssetType",
+    "FinanceAccount",
+    "FinanceAccountProperties",
+    "FinanceAccountType",
+    "RawTransfer",
+    "Transfer",
+    "Wallet",
+    "WalletType",
+]
